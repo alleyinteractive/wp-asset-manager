c10d2e932d4090153f84adb75766f150d259028b
srtfisher <srtfisher@users.noreply.github.com>
2025-01-15 21:16:19 +0000

    Built changes for v1.4.2 from production 1f71b35ade5113433a1043f29df05aaee411a14d

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alleyinteractive/wp-asset-manager',
+        'pretty_version' => 'dev-production',
+        'version' => 'dev-production',
+        'reference' => '1f71b35ade5113433a1043f29df05aaee411a14d',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alleyinteractive/wp-asset-manager' => array(
+            'pretty_version' => 'dev-production',
+            'version' => 'dev-production',
+            'reference' => '1f71b35ade5113433a1043f29df05aaee411a14d',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
