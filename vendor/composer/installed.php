4c537e357b9b35cfe7eb1c574db18d4eadbee36c
renatonascalves <renatonascalves@users.noreply.github.com>
2025-01-27 15:01:09 +0000

    Built changes for v1.4.3 from production d4abf62d7ae8bdbcb86ec1382de09a27865e2377

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alleyinteractive/wp-asset-manager',
+        'pretty_version' => 'dev-production',
+        'version' => 'dev-production',
+        'reference' => 'd4abf62d7ae8bdbcb86ec1382de09a27865e2377',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alleyinteractive/wp-asset-manager' => array(
+            'pretty_version' => 'dev-production',
+            'version' => 'dev-production',
+            'reference' => 'd4abf62d7ae8bdbcb86ec1382de09a27865e2377',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
