bbf6e1995d1c60121796018bcc776edd42470f57
mslinnea <mslinnea@users.noreply.github.com>
2025-07-14 17:09:40 +0000

    Built changes from production 47ccb67297c01fa83a284f30cdcc585df8e78d45

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'alleyinteractive/wp-asset-manager',
+        'pretty_version' => 'dev-production',
+        'version' => 'dev-production',
+        'reference' => '47ccb67297c01fa83a284f30cdcc585df8e78d45',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'alleyinteractive/wp-asset-manager' => array(
+            'pretty_version' => 'dev-production',
+            'version' => 'dev-production',
+            'reference' => '47ccb67297c01fa83a284f30cdcc585df8e78d45',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
